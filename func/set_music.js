fb42ba2f0427422e4f42dfd7649cca333dba7942
WEDNES9 <117744705+WEDNES9@users.noreply.github.com>
2022-12-09 23:03:54 +0900

    Update set_music.js

--- func/set_music.js
+++ func/set_music.js
@@ -1,24 +1,12 @@
+
 //음악의 재생 버튼 클릭 시 파라미터를 가져와서 미니플레이어에 붙이기
 //현재 사용자의 플레이리스트에 곡 추가 및 플레이카운트 증가
 function set_music(name, artist, link, album, num) {
 
-    var url = link.replace("https://www.youtube.com/embed/", "");
+    $("#musicname").text(name);
+    $("#musicartist").text(artist);
+    $("#musicimg").attr("src", album);
 
-    $.ajax({
-        type: "POST",
-        url: "../func/SessionManager.jsp" +
-            "?name="+name+"&artist="+artist+"&link="+url+"&album="+album+"",
-        contentType: "text; charset=UTF-8",
-        success: function () {
-
-            $("#musicname").text(name);
-            $("#musicartist").text(artist);
-            $("#musicimg").attr("src", album);
-
-        }, error: function () {
-            alert("Error..");
-        }
-    })
 
     $.ajax({
         type: "POST",
@@ -27,30 +15,54 @@ function set_music(name, artist, link, album, num) {
         success: function () {
 
         },error: function () {
-            alert("Error...");
+            alert("Music_playlist.jsp : Error..");
         }
     })
 
+    $.ajax({
+        type: "POST",
+        url: "../func/Music_Count.jsp?num="+num+"",
+        contentType: "text; charset=UTF-8",
+        success: function () {
+
+        },error: function () {
+            alert("Music_Count.jsp : Error..");
+        }
+    })
 
 }
-//재생 제어
-//재생 시 재생버튼, 일시정지 시 일시정지 버튼 나오도록 함
-function playmusic() {
 
-    //playVideo=재생, pauseVideo=일시정지, stopVideo=정지
+function play_new(url) {
 
-    if (playnstop.value === "now_pause") {   //일시정지
-        playnstop.setAttribute("value", "now_play");
-        playnstop.setAttribute("src", "../homepageimg/play_btn.png");
+    var link = url + '?enablejsapi=1&version=3&playerapiid=ytplayer';
 
-        //player.pauseVideo();
+    document.getElementById('player').remove();
 
-    } else {    //재생
-        playnstop.setAttribute("value", "now_pause");
-        playnstop.setAttribute("src", "../homepageimg/pause_btn.png");
+    var player_new = document.createElement('iframe');
+    player_new.setAttribute('id', 'player');
+    player_new.setAttribute('width', 0);
+    player_new.setAttribute('height', 0);
+    player_new.setAttribute('src', link);
+    document.body.appendChild(player_new);
+    document.getElementById('playnstop').setAttribute('value', 'now_pause');
+    document.getElementById('playnstop').setAttribute('src', '../homepageimg/play_btn.png');
+    player_new.hidden;
 
-        //player.playVideo();
+}
 
+function play_btn() {
+
+    var nowplayer = document.getElementById('player');
+    var btn = document.getElementById("playnstop");
+
+    if (btn.getAttribute('value') === 'now_pause') {
+        btn.setAttribute('value', 'now_play');
+        btn.setAttribute('src', '../homepageimg/pause_btn.png');
+        nowplayer.contentWindow.postMessage('{"event":"command","func":"' + 'playVideo' + '","args":""}', '*');
+    } else {
+        btn.setAttribute('value', 'now_pause');
+        btn.setAttribute('src', '../homepageimg/play_btn.png');
+        nowplayer.contentWindow.postMessage('{"event":"command","func":"' + 'pauseVideo' + '","args":""}', '*');
     }
 
 }
